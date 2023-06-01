1a280058e2a0779045dc2eda34cdf0697aceee1d
karthik-mohan23 <132973611+karthik-mohan23@users.noreply.github.com>
2023-06-01 20:53:41 +0530

    created an object constructor

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("ready");
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.report = function () {
+    return `${title} by ${author},${pages} pages, ${read}`;
+  };
+}
+const book1 = new Book("Meditations", "Karthik", "205", "read");
+console.log(book1.report());
